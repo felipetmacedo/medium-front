1923b9a0f8a8b8706f0fca7b4c143c751265e490
felipetmacedo <ftm2@cin.ufpe.br>
2024-06-18 22:45:23 -0300

    refactor: Remove unused files and update UI styles
    Remove unused files: customTemplate.html, RecoveryService.js, image-page.html, updateUserController.js, and all-user-movies.html. Update UI styles in recover.css, update-user.css, every-user.css, cadastro.css, manage-movies.css, and geral.css for improved visual consistency.
    
    - Remove unused files: customTemplate.html, RecoveryService.js, image-page.html, updateUserController.js, and all-user-movies.html
    - Update UI styles in recover.css, update-user.css, every-user.css, cadastro.css, manage-movies.css, and geral.css

--- js/app.js
+++ js/app.js
@@ -29,66 +29,6 @@ myApp.config(function ($stateProvider, $httpProvider) {
       templateUrl: "view/show-post.html",
       controller: "showPostController",
       onEnter: isAuthorized,
-    })
-    .state({
-      name: "profile",
-      url: "/profile",
-      templateUrl: "view/profile.html",
-      controller: "profileController",
-      onEnter: isAuthorized,
-    })
-    .state({
-      name: "search-user-profile",
-      url: "/profile/:id",
-      templateUrl: "view/profile.html",
-      controller: "profileController",
-      onEnter: isAuthorized,
-    })
-    .state({
-      name: "update-user",
-      url: "/profile/update",
-      templateUrl: "view/update-user.html",
-      controller: "updateUserController",
-      onEnter: isAuthorized,
-    })
-    .state({
-      name: "create-movie",
-      url: "/movies/manage",
-      templateUrl: "view/manage-movie.html",
-      controller: "manageMovieController",
-      onEnter: isAuthorized,
-    })
-    .state({
-      name: "update-movie",
-      url: "/movies/manage/:id",
-      templateUrl: "view/manage-movie.html",
-      controller: "manageMovieController",
-      onEnter: isAuthorized,
-    })
-    .state({
-      name: "every-user",
-      url: "/profile/all",
-      templateUrl: "view/every-user.html",
-      controller: "everyUserController",
-      onEnter: isAuthorized,
-    })
-    .state({
-      name: "recover",
-      url: "/password/recover",
-      templateUrl: "view/password-recover.html",
-      controller: "passwordRecoverController",
-    })
-    .state({
-      name: "change-password",
-      url: "/change-password",
-      templateUrl: "view/change-password.html",
-      controller: "changePasswordController",
-    })
-    .state({
-      name: "all-user-movies",
-      url: "/profile/movies",
-      templateUrl: "view/all-user-movies.html",
-      controller: "profileController",
     });
 });
 
--- js/services/RecoveryService.js
+++
@@ -1,5 +0,0 @@
-myApp.service("RecoveryService", function($http) {
-    this.recovery = (data) =>  $http.post(`${baseUrl}recovery_password/`, data);
-    this.validateToken = (token) =>  $http.get(`${baseUrl}validate-token-password/${token}`);
-    this.changePassword = (token, data) =>  $http.put(`${baseUrl}change-password/${token}`, data);
-  })
