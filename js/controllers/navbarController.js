6f6876925fb7bbdd93b30e8ab72661c293f1f720
felipetmacedo <ftm2@cin.ufpe.br>
2024-06-18 22:31:42 -0300

    refactor: Update page title and UI styles for consistency
    Update the page title in index.html to "Amigo Medium" for better branding. Additionally, update the UI styles in recover.css, update-user.css, every-user.css, cadastro.css, manage-movies.css, and geral.css to improve visual consistency across the application.
    
    - Update the page title in index.html
    - Update the UI styles in recover.css, update-user.css, every-user.css, cadastro.css, manage-movies.css, and geral.css

--- js/controllers/navbarController.js
+++ js/controllers/navbarController.js
@@ -10,10 +10,9 @@ myApp.controller("navbarController", [
       password: "",
     };
     $scope.formData = {
-      username: "",
+      name: "",
       email: "",
       password: "",
-      confirmPassword: "",
     };
 
     const login = () => {
@@ -46,15 +45,21 @@ myApp.controller("navbarController", [
     const create = () => {
       return UserService.create($scope.formData)
         .then(() => {
-          $scope.err = false;
-          $state.go("home");
+          Swal.fire({
+            title: "Conta criada com sucesso!",
+            text: "Faça login para continuar!",
+            icon: "success",
+          });
+          $scope.formData.name = "";
+          $scope.formData.email = "";
+          $scope.formData.password = "";
+          $scope.formData.confirmPassword = "";
         })
         .catch(() => {
           Swal.fire({
-            title: "dados inválidos",
+            title: "Dados Inválidos!",
             text: "Verifique as suas informações!",
             icon: "error",
-            confirmButtonColor: "#04052e",
           });
           $scope.err = true;
           localStorage.clear();
@@ -110,8 +115,37 @@ myApp.controller("navbarController", [
         login();
       }
     };
-    $scope.create = create;
-    $scope.login = login;
+
+    $scope.triggerRegister = async function () {
+      const { value: formValues } = await Swal.fire({
+        title: "Create Account",
+        html: `
+          <input id="swal-input1" class="swal2-input" placeholder="Name">
+          <input id="swal-input2" class="swal2-input" placeholder="Email">
+          <input id="swal-input3" class="swal2-input" placeholder="Password">
+        `,
+        focusConfirm: false,
+        showCancelButton: true,
+        confirmButtonText: "Confirm",
+        cancelButtonText: "Cancel",
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        reverseButtons: true,
+        preConfirm: () => {
+          return [
+            document.getElementById("swal-input1").value,
+            document.getElementById("swal-input2").value,
+            document.getElementById("swal-input3").value,
+          ];
+        },
+      });
+      if (formValues) {
+        $scope.formData.name = formValues[0];
+        $scope.formData.email = formValues[1];
+        $scope.formData.password = formValues[2];
+        create();
+      }
+    };
     $scope.logOut = logOut;
   },
 ]);
